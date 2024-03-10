0dae422715ab591517109a8fd4661cf325b38613
AGA2020 <hasan9u@gmail.com>
2024-03-10 20:38:44 +0300

    Proje tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double sayi, kdvTutar, toplam;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Fiyatı giriniz : " );
+        sayi = input.nextDouble();
+
+        double kdvdegiskenoran = (sayi >= 0 && sayi <= 1000) ? 0.18 : 0.08;
+
+        kdvTutar = sayi * kdvdegiskenoran;
+        toplam = sayi + kdvTutar;
+
+        System.out.println("Kdv'siz tutar:" + sayi);
+        System.out.println("Kdv oranı:" + kdvdegiskenoran);
+        System.out.println("Kdv'li tutar:" + toplam);
+        System.out.print("KDV tutarı:" + kdvTutar);
     }
 }
